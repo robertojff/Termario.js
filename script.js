63a102d5575d247832791f2226a6b7125c2ebcd5
robertojff <robertojff3333@gmail.com>
2023-12-26 18:53:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let pontosPalmeiras = 31;
+let tipo = pontosPalmeiras > 21 ? 'premium':'comum';
+console.log(tipo);
+
+let pontosCorinthians = 28;
+let tipoDois = pontosCorinthians > 28 ? 'premium':'comum';
+console.log(tipoDois);
+
+let pontosVasco = 4;
+let tipoTrês = pontosVasco > 2 ? 'premium':'comum';
+console.log(tipoTrês);
+
+let pontosSãoPaulo = 60;
+let tipoQuatro = pontosSãoPaulo > 61 ? 'premium':'comum';
+console.log(tipoQuatro);
+
+let pontosSantos = 55;
+let tipoQuinto = pontosSantos > 45 ? 'premium':'comum';
+console.log(tipoQuinto);
+
+
+
+
